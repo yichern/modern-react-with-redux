ce7ed8584d12679a3967143814a52468e5e2902e
tanyichern <tanyichern@gmail.com>
2020-06-13 00:41:05 +0800

    Section 1: Let\'s Dive In\!

---
+++ jsx/src/index.js
@@ -0,0 +1,11 @@
+// import React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
+
+// create a react component
+const App = () => {
+  return <div> Hi there! </div>;
+};
+
+// take react component, show on screen
+ReactDOM.render(<App />, document.querySelector("#root"));
