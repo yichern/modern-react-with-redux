cf5dee8565af745eb7bfceed4fcaee29b1da2326
tanyichern <tanyichern@gmail.com>
2020-06-19 18:51:03 +0800

    replace pics with images

--- pics/src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-    Authorization: 'Client-ID T2VsZnnQkG4zoyTnUcIgD0maTnlKktNMQBDLR_tFcbg',
-  },
-});
