fa462ed26abcfd68bc5efbf3e0763cc023cbda6d
tanyichern <tanyichern@gmail.com>
2020-06-19 18:06:30 +0800

    Section 26: More Fun with Hooks

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,41 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 import SeasonDisplay from './components/SeasonDisplay';
 import Spinner from './components/Spinner';
+import useLocation from './useLocation';
 
-class App extends React.Component {
-  state = {
-    lat: null,
-    errorMessage: '',
-  };
+const App = () => {
+  const [lat, errorMessage] = useLocation();
 
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
-    );
+  let content;
+  if (errorMessage) {
+    content = <div>Error: {errorMessage}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />;
+  } else {
+    content = <Spinner message="Please accept location request" />;
   }
 
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    }
-
-    if (this.state.lat && !this.state.errorMessage) {
-      return <SeasonDisplay lat={this.state.lat} />;
-    }
-
-    return <Spinner message="Please accept location request" />;
-  }
-
-  render() {
-    return <div>{this.renderContent()}</div>;
-  }
-}
+  return <div className="border red">{content}</div>;
+};
 
 ReactDOM.render(<App />, document.querySelector('#root'));
---
+++ seasons/src/useLocation.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+export default () => {
+  const [lat, setLat] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLat(position.coords.latitude);
+      },
+      (err) => {
+        setErrorMessage(err.message);
+      }
+    );
+  }, []);
+
+  return [lat, errorMessage];
+};
